cfd6694c1d5d20da8a2a26249aa5e8e1923322ba
Ssnehnil <ghosh.s@etoncollege.org.uk>
2022-02-02 23:05:28 +0000

    Adding basic model for division and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Ssnehnil Ghosh on 02/02/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
