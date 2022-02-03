240bd6c859377fbab4213084777587a9b518be3c
Ssnehnil <ghosh.s@etoncollege.org.uk>
2022-02-03 06:56:45 +0000

    Completed Parts 1 & 2 of the Absence Recorder

---
+++ AbsenceRecorder/View/DivisionItem.swift
@@ -0,0 +1,25 @@
+//
+//  DivisionItem.swift
+//  AbsenceRecorder
+//
+//  Created by Ssnehnil Ghosh on 03/02/2022.
+//
+
+import SwiftUI
+
+struct DivisionItem: View {
+    let division: Division
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "\(division.students.count).circle")
+            Text("\(division.code)")
+        }
+    }
+}
+
+struct DivisionItem_Previews: PreviewProvider {
+    static var previews: some View {
+        DivisionItem(division: Division(code: "vBY-1"))
+    }
+}
