cca3280cd9556776416bd045f70aed2bdda96dac
Ssnehnil <ghosh.s@etoncollege.org.uk>
2022-02-07 22:02:15 +0000

    Absence Recorder Part 3

---
+++ AbsenceRecorder/View/RootTabView.swift
@@ -0,0 +1,31 @@
+//
+//  RootTabView.swift
+//  AbsenceRecorder
+//
+//  Created by Ssnehnil Ghosh on 07/02/2022.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        TabView {
+            DivisionsView()
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("Absences")
+                }
+            StatisticsView()
+                .tabItem {
+                    Image(systemName: "chart.xyaxis.line")
+                    Text("Statistics")
+                }
+        }
+    }
+}
+
+struct RootTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootTabView()
+    }
+}
